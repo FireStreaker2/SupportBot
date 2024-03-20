9818d33c75c76675a980a55809659f8579dab2ad
FireStreaker2 <michaelccc2019@outlook.com>
2024-03-19 18:45:41 -0700

    material ui

--- dashboard/src/pages/_app.tsx
+++ dashboard/src/pages/_app.tsx
@@ -1,4 +1,7 @@
-import "@/styles/globals.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
